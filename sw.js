1fd235f9c135cfa61a32adf4c2c0cc7a14876a13
dqhalhudasukoharjo-a11y <dqhalhudasukoharjo@gmail.com>
2026-09-24 09:23:43 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = "dqh-alhuda-v2";
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./index.html", "./logo.png", "./manifest.webmanifest"])));
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;
+  if (e.request.mode === "navigate") {
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const cl = res.clone(); caches.open(CACHE).then(c => c.put("./index.html", cl)); return res;
+      }).catch(() => caches.match("./index.html"))
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+        const cl = res.clone(); caches.open(CACHE).then(c => c.put(e.request, cl)); return res;
+      }))
+    );
+  }
+});
