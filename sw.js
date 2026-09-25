2a7143f901224e7a1b04d4901c53ede985a9c9c9
dqhalhudasukoharjo-a11y <dqhalhudasukoharjo@gmail.com>
2026-09-25 15:34:08 +0700

    Update cache version to dqh-alhuda-v10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dqh-alhuda-v9";
+const CACHE = "dqh-alhuda-v10";
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./index.html", "./logo.png", "./manifest.webmanifest"])));
   self.skipWaiting();
